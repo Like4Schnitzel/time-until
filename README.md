dc847aca5a9ff791b31c125dd60a9cc4490075c6
Laura <rw21062006@gmail.com>
2025-07-01 01:03:12 +0200

    add units

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 Simple C++ REST API server that returns the time between now and a given date.
 
 ## Usage
-Sending a GET request to localhost:8080/`YYYY-MM-DDTHH:MM:SS` will respond with the amount of days remaining as an int.
+Sending a GET request to localhost:8080/`YYYY-MM-DD[THH:MM:SS]` will respond with the amount of days remaining as an int.
+
+The port can be changed by specifying it in the first CLI arg, i.e. `time-until 8081`.
+
+You can specify any of the following units in the query like so: localhost:8080/`YYYY-MM-DD[THH:MM:SS]`?`UNIT`:
+- `y`, `year`, `years`
+- `M`, `month`, `months`
+- `d`, `day`, `days`
+- `h`, `hour`, `hours`
+- `m`, `minute`, `minutes`
+- `s`, `second`, `seconds`
+besides `M` and `m`, none of the units are case sensitive.
+
+### Examples:
+`curl localhost:8080/2022-12-24`
+`curl localhost:8080/2022-12-24T05:00:00`
+`curl localhost:8080/2025-07-28T7:15:00?MINUTES`
+`curl localhost:8080/2006-11-19?y`
 
 ## Building
 [Install the C++ REST SDK](https://github.com/microsoft/cpprestsdk#getting-started).
--- src/handler.cpp
+++ src/handler.cpp
@@ -15,6 +15,14 @@ pplx::task<void> Handler::close()
     return listener.close();
 }
 
+inline string toLower(const string& data)
+{
+    string tmp = data;
+    std::transform(tmp.begin(), tmp.end(), tmp.begin(),
+    [](unsigned char c){ return std::tolower(c); });
+    return tmp;
+}
+
 void Handler::handleGet(http_request message)
 {
     auto paths = uri::split_path(uri::decode(message.relative_uri().path()));
@@ -29,8 +37,40 @@ void Handler::handleGet(http_request message)
         const auto timestamp = DateTimeParser::parseISOTimestamp(paths[0]);
         const auto now = time(NULL);
         const auto diff = timestamp - now;
-        const long diffDays = round(diff / (double)(60 * 60 * 24));
-        message.reply(status_codes::OK, diffDays);
+        long unitDiff = NULL;
+
+        const string query = message.request_uri().query();
+        const string lowerQuery = toLower(query);
+        if (query.empty() || lowerQuery == "d" || lowerQuery == "day" || lowerQuery == "days")
+        {
+            unitDiff = round(diff / (double)(60 * 60 * 24));
+        }
+        else if (query == "M" || lowerQuery == "month" || lowerQuery == "months")
+        {
+            unitDiff = round(diff / (60 * 60 * 24 * 30.4375));
+        }
+        else if (lowerQuery == "y" || lowerQuery == "year" || lowerQuery == "years")
+        {
+            unitDiff = round(diff / (60 * 60 * 24 * 365.25));
+        }
+        else if (lowerQuery == "h" || lowerQuery == "hour" || lowerQuery == "hours")
+        {
+            unitDiff = round(diff / (double)(60 * 60));
+        }
+        else if (query == "m" || lowerQuery == "minute" || lowerQuery == "minutes")
+        {
+            unitDiff = round(diff / (double)60);
+        }
+        else if (lowerQuery == "s" || lowerQuery == "second" || lowerQuery == "seconds")
+        {
+            unitDiff = diff;
+        }
+        else
+        {
+            throw invalid_argument("Unit \"" + query + "\" not recognized.");
+        }
+
+        message.reply(status_codes::OK, unitDiff);
     }
     catch (invalid_argument e)
     {
